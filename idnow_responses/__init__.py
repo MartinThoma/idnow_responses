b68f7be488f118ee52fac120c63313783b6bcb9d
Martin Thoma <info@martin-thoma.de>
2022-10-19 21:59:46 +0200

    Run pre-commit

--- idnow_responses/__init__.py
+++ idnow_responses/__init__.py
@@ -1,6 +1,8 @@
+import re
+
 import pytest
 import responses
-import re
+
 
 @pytest.fixture
 def idnow_responses(request=None):
@@ -8,7 +10,9 @@ def idnow_responses(request=None):
     with responses.RequestsMock(assert_all_requests_are_fired=False) as rsps:
         rsps.add(
             responses.POST,
-            re.compile(f"https://gateway.test.idnow.de/api/v1/{company_id}/identifications/.*"),
+            re.compile(
+                f"https://gateway.test.idnow.de/api/v1/{company_id}/identifications/.*"
+            ),
             json={"id": "new-idnow-id"},
             status=200,
         )
